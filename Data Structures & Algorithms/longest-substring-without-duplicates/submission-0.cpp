7c800574ad631fead0497e066e6e86f8dc2864a7
anamikaydv745-sketch <anamikaydv745@gmail.com>
2026-09-14 18:18:14 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        set<char> st;
+
+        int i = 0;
+        int maxlen = 0;
+
+        for(int j = 0; j < s.size(); j++) {
+
+            while(st.find(s[j]) != st.end()) {
+                st.erase(s[i]);
+                i++;
+            }
+
+            st.insert(s[j]);
+
+            maxlen = max(maxlen, j - i + 1);
+        }
+
+        return maxlen;
+    }
+};
